36a61fdb2be8f0fe32dd0bb1a9dee7469aeeefdf
timh <tim@muton.co.uk>
2013-04-27 07:01:29 +0100

    Freemind docs - brainstorming minimalism, and the game idea.

---
+++ docs/MINIMALIST INVADERS FROM OUTER SPACE.mm
@@ -0,0 +1,103 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1367033109098" ID="ID_986778724" LINK="minimalism.mm" MODIFIED="1367040559971" TEXT="MINIMALIST INVADERS FROM OUTER SPACE">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1367041384308" ID="ID_553339131" MODIFIED="1367041390671" POSITION="left" TEXT="initial thoughts">
+<node CREATED="1367033120097" ID="ID_74812699" MODIFIED="1367033141812" TEXT="Must combat aliens by creating crap"/>
+<node CREATED="1367033952208" ID="ID_138807646" MODIFIED="1367033956879" TEXT="maybe mud"/>
+<node CREATED="1367033957527" ID="ID_287948622" MODIFIED="1367033960050" TEXT="move furniture?"/>
+<node CREATED="1367033967080" ID="ID_830714653" MODIFIED="1367033971814" TEXT="pull out toilet paper"/>
+<node CREATED="1367033153483" ID="ID_1155214079" MODIFIED="1367033179932" TEXT="&quot;reduce, reduce&quot;, &quot;simplify, simplify&quot; - dalek voice"/>
+<node CREATED="1367033228093" ID="ID_540066945" MODIFIED="1367033379793" TEXT="Must increase environmental entropy/disorder/clutter"/>
+</node>
+<node CREATED="1367033437259" ID="ID_1216509324" MODIFIED="1367033438268" POSITION="left" TEXT="idea">
+<node CREATED="1367033400699" MODIFIED="1367033435062" TEXT="Aliens have invaded, and created nice minimalist living environments for themselves"/>
+<node CREATED="1367033446855" MODIFIED="1367033467260" TEXT="You must sneak around and impose clutter by dropping things, moving stuff around, etc"/>
+<node CREATED="1367033467815" MODIFIED="1367033487783" TEXT="When too much is out of place, aliens will leave in disgust"/>
+<node CREATED="1367033488307" MODIFIED="1367033492739" TEXT="maybe argue with each-other, etc"/>
+</node>
+<node CREATED="1367041510519" ID="ID_207454851" MODIFIED="1367041511879" POSITION="left" TEXT="story">
+<node CREATED="1367041513545" ID="ID_1569211033" MODIFIED="1367041544659" TEXT="An alien couple have moved in by obliterating native lifeforms (humans) and building themselves a dream-house. Parallels of city folk moving to the countryside."/>
+<node CREATED="1367041554892" ID="ID_1769684052" MODIFIED="1367041597349" TEXT="They are super-minimalists and react to anything out of place with a shout of &quot;CLUTTER&quot; and an insatiable urge to tidy up."/>
+<node CREATED="1367041715139" ID="ID_725186654" MODIFIED="1367041721568" TEXT="initial cutscene">
+<node CREATED="1367041722508" ID="ID_1550501887" MODIFIED="1367041857847" TEXT="Sruffy looking human news reporter is standing outside the outer boundary wall, doing a piece to camera - initial exposition"/>
+<node CREATED="1367041858875" ID="ID_1757817393" MODIFIED="1367041885257" TEXT="Suddenly there&apos;s a shout of CLUTTER!! and the reporter is vaporised"/>
+<node CREATED="1367042022695" ID="ID_133165983" MODIFIED="1367042060353" TEXT="cut to inside the house: DayVIDD is admiring his &quot;Reducer&quot; death-ray; &quot;Such an elegant device!&quot; - looks like Apple might have made it"/>
+<node CREATED="1367042070773" ID="ID_17783532" MODIFIED="1367042088286" TEXT="FiYoNARR: Yes! And no mess to clean up afterwards!"/>
+<node CREATED="1367042115728" ID="ID_738979271" MODIFIED="1367042154342" TEXT="back outside: Cameraman is extremely annoyed &quot;oh crap, she had the car keys!!&quot; - referring to his disappeared colleague"/>
+<node CREATED="1367042155846" ID="ID_1865030028" MODIFIED="1367042190315" TEXT="&quot;Might as well get rid of these tossers while I&apos;m here!&quot;"/>
+</node>
+<node CREATED="1367042212949" ID="ID_1822805989" MODIFIED="1367042214275" TEXT="Gameplay">
+<node CREATED="1367042215053" ID="ID_1136083442" MODIFIED="1367042220290" TEXT="Player must sneak around without being seen"/>
+<node CREATED="1367042220831" ID="ID_1815585079" MODIFIED="1367042235568" TEXT="The aliens will be driven mad by disorder"/>
+<node CREATED="1367042236347" ID="ID_706234971" MODIFIED="1367042253233" TEXT="So you have to increase the clutter level of their home to an unacceptable level"/>
+</node>
+</node>
+<node CREATED="1367040584763" ID="ID_1725094426" MODIFIED="1367040636713" POSITION="right" TEXT="unnecessary things">
+<node CREATED="1367040596852" ID="ID_372254085" MODIFIED="1367040612569" TEXT="kitchen equipment - microwave, food processor, juicer, etc"/>
+<node CREATED="1367040672358" ID="ID_69614916" MODIFIED="1367040674388" TEXT="remote controls"/>
+<node CREATED="1367041644401" ID="ID_1798195299" MODIFIED="1367041645868" TEXT="ornaments"/>
+<node CREATED="1367041646222" ID="ID_1224191941" MODIFIED="1367041665203" TEXT="musical instruments"/>
+<node CREATED="1367041666502" ID="ID_644647503" MODIFIED="1367041680137" TEXT="family photos"/>
+<node CREATED="1367041684873" ID="ID_1327799477" MODIFIED="1367041686291" TEXT="cosmetics"/>
+</node>
+<node CREATED="1367040640704" ID="ID_33450066" MODIFIED="1367040643868" POSITION="right" TEXT="sabotage wedding list"/>
+<node CREATED="1367040646897" ID="ID_114290463" MODIFIED="1367040659441" POSITION="right" TEXT="change the music from phillip glass to something awful"/>
+<node CREATED="1367040663517" ID="ID_958126677" MODIFIED="1367040665109" POSITION="right" TEXT="disco lights"/>
+<node CREATED="1367040690626" ID="ID_1220163163" MODIFIED="1367040719093" POSITION="right" TEXT="&apos;MA-JI&apos; sale, better than half price offers, everything must go - tempt wife">
+<node CREATED="1367041122538" ID="ID_1512707060" MODIFIED="1367041133579" TEXT="she leaves (reduces cleanup speed)"/>
+<node CREATED="1367041134082" ID="ID_637043207" MODIFIED="1367041144984" TEXT="comes back with loads of awesome minimal stuff! doh"/>
+</node>
+<node CREATED="1367040872662" ID="ID_1547380979" MODIFIED="1367040887265" POSITION="right" TEXT="get the key to the &quot;entropy cupboard&quot; - where they put all their crap in order to maintain order elsewhere">
+<node CREATED="1367040930241" ID="ID_945452230" MODIFIED="1367040944603" TEXT="bigger on the inside - switch to separate screen? tardis-like"/>
+</node>
+<node CREATED="1367040974534" ID="ID_756433822" MODIFIED="1367040979243" POSITION="right" TEXT="break the dishwasher">
+<node CREATED="1367040980307" ID="ID_547447578" MODIFIED="1367041000578" TEXT="but it&apos;s ok, they only have two plates, two bowls, etc">
+<node CREATED="1367041002180" ID="ID_313623770" MODIFIED="1367041011638" TEXT="so get the rest of the crockery from the entropy cupboard"/>
+<node CREATED="1367041012024" ID="ID_1909743708" MODIFIED="1367041021369" TEXT="and suddenly the entire kitchen is a mess"/>
+</node>
+</node>
+<node CREATED="1367041063468" ID="ID_1544615814" MODIFIED="1367041070310" POSITION="right" TEXT="aliens constantly try to clean up">
+<node CREATED="1367041071390" ID="ID_1211910456" MODIFIED="1367041080169" TEXT="give them some logic, pathfinding, etc"/>
+<node CREATED="1367041080935" ID="ID_1126395523" MODIFIED="1367041092054" TEXT="you win when the level of disorder becomes unbearable"/>
+<node CREATED="1367041096350" ID="ID_1939782281" MODIFIED="1367041117149" TEXT="possible to lose if they tidy up the big disruptions faster than you can create them"/>
+</node>
+<node CREATED="1367040745080" ID="ID_1943093709" MODIFIED="1367040751108" POSITION="right" TEXT="player character">
+<node CREATED="1367040752086" ID="ID_1815168839" MODIFIED="1367040752984" TEXT="beard"/>
+<node CREATED="1367040753953" ID="ID_1827673716" MODIFIED="1367040756408" TEXT="beer-hat"/>
+<node CREATED="1367040764741" ID="ID_1968836049" MODIFIED="1367040766579" TEXT="combats"/>
+<node CREATED="1367040767150" ID="ID_1508278982" MODIFIED="1367040768580" TEXT="rucksack"/>
+<node CREATED="1367040769288" ID="ID_1586927137" MODIFIED="1367040770672" TEXT="tool belt"/>
+<node CREATED="1367040784475" ID="ID_1579325756" MODIFIED="1367040787514" TEXT="gaudy colours"/>
+<node CREATED="1367040794780" ID="ID_1900465329" MODIFIED="1367040804138" TEXT="piercings &amp; tattoos"/>
+</node>
+<node CREATED="1367041238545" ID="ID_301389154" MODIFIED="1367041240798" POSITION="right" TEXT="alien characters">
+<node CREATED="1367041241797" ID="ID_1244079465" MODIFIED="1367041243172" TEXT="husband">
+<node CREATED="1367041904149" ID="ID_870790576" MODIFIED="1367041937892" TEXT="DayVIDD">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1367041249469" ID="ID_964267577" MODIFIED="1367041271140" TEXT="attacks"/>
+<node CREATED="1367041293376" ID="ID_1280445495" MODIFIED="1367041307876" TEXT="always obeys wife - stops what he&apos;s doing if he&apos;s called (resets cleanup activity)"/>
+<node CREATED="1367041316541" ID="ID_1397828622" MODIFIED="1367041354433" TEXT="ruthlessly minimalist"/>
+</node>
+<node CREATED="1367041243728" ID="ID_1031301017" MODIFIED="1367041245779" TEXT="wife">
+<node CREATED="1367041918049" ID="ID_843543701" MODIFIED="1367041953166" TEXT="FiYoNARR">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1367041272709" ID="ID_286629546" MODIFIED="1367041279360" TEXT="calls husband if she sees you"/>
+<node CREATED="1367041284484" ID="ID_55556860" MODIFIED="1367041331736" TEXT="wishful minimalist, easily tempted to clutter"/>
+</node>
+</node>
+<node CREATED="1367041167634" ID="ID_1098130624" MODIFIED="1367041179499" POSITION="right" TEXT="if seen">
+<node CREATED="1367041180382" ID="ID_1402402170" MODIFIED="1367041207818" TEXT="v1: insta-death by Reducer"/>
+<node CREATED="1367041208520" ID="ID_38960799" MODIFIED="1367041226729" TEXT="v2: chase/seek behaviour - no way I can do this for LD"/>
+</node>
+<node CREATED="1367041426408" ID="ID_1218059896" MODIFIED="1367041433516" POSITION="right" TEXT="location">
+<node CREATED="1367041434510" ID="ID_620902395" MODIFIED="1367041455679" TEXT="stark modern 1-level house, white and light grey"/>
+<node CREATED="1367041456127" ID="ID_1438541843" MODIFIED="1367041472026" TEXT="bright sunlight streams in through the windows"/>
+<node CREATED="1367041472907" ID="ID_1558570598" MODIFIED="1367041477243" TEXT="patio outside"/>
+<node CREATED="1367041477809" ID="ID_75225208" MODIFIED="1367041491502" TEXT="arranged in a ring - few doors, very open"/>
+<node CREATED="1367041495730" ID="ID_1216865954" MODIFIED="1367041497924" TEXT="fairly large"/>
+</node>
+</node>
+</map>
