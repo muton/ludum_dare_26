e97a26d9d860960e40dfeb5f1676fa228bb00e25
timh <tim@muton.co.uk>
2013-04-28 19:42:02 +0100

    Entropy stack graphic
    tweaked disorder values

--- source/muton/ld26/Config.hx
+++ source/muton/ld26/Config.hx
@@ -169,7 +169,7 @@ class Config {
 		var colour = 0xFFF0F0F0;
 		scenery = new Hash<SceneryInfo>();
 		var itemList = [
-			["dustbin", "3x3", "dustbin_sheet.png", "y", "10"],	// knock it over
+			["dustbin", "3x3", "dustbin_sheet.png", "y", "5"],	// knock it over
 			["spaceship", "4x13", "spaceship.png"],
 			["pond", "5x45", "pond.png"],
 			["outdoor_seat", "3x3", "outdoor_seat.png"],
@@ -182,10 +182,10 @@ class Config {
 			["shower", "13x5", "shower.png"],
 			["basin", "2x3", "basin_sheet.png", "y", "5"],  // rearrange toiletries
 			["bed", "15x10", "bed_sheet.png", "y", "5"], // unmake
-			["entropy_stack", "4x4", "", "y", "10"], //special
+			["entropy_stack", "4x4", "entropy_stack_sheet.png", "y", "7"], //special
 			["kitchen_sink", "4x5", "sink_sheet.png", "y", "8"], // fill with pots and pans
-			["hi_fi", "3x3", "hi_fi_sheet.png", "y", "15"], // change the music
-			["dish_washer", "1x5", "", "y", "20"], // sabotage
+			["hi_fi", "3x3", "hi_fi_sheet.png", "y", "12"], // change the music
+			["dish_washer", "1x5", "", "y", "10"], // sabotage
 			["bin", "2x2", "bin_sheet.png", "y", "5"] // tip
 		];
 		
